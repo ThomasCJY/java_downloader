754da3aba115a23ba3bc2b3d69400d96ff60e62e
ThomasCJY <460540025@qq.com>
2016-01-26 15:17:52 -0500

    UI downloader--filechooser

--- download/src/UIDownloader.java
+++ download/src/UIDownloader.java
@@ -2,26 +2,56 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Kayuk on 1/10/16.
  */
 public class UIDownloader extends JPanel
         implements ActionListener{
-    private JButton dirButton;
+    JButton dirButton;
+    JFileChooser fc;
+    JTextField jfield;
+    File savePath;
 
     public UIDownloader(){
-        dirButton = new JButton("Location");
+        savePath = new File(".");
+
+        jfield = new JTextField(20);
+        jfield.setEditable(false);
+
+        fc = new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        dirButton = new JButton("Choose Path");
+        dirButton.addActionListener(this);
+
+
+
         JPanel buttonPanel = new JPanel();
+
         buttonPanel.add(dirButton);
+        buttonPanel.add(jfield);
         add(buttonPanel);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-//        if (e.getSource() == dirButton){
-//
-//        }
+        if (e.getSource() == dirButton){
+            int returnVal = fc.showOpenDialog(UIDownloader.this);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                savePath = fc.getSelectedFile();
+                //This is where a real application would open the file.
+                Logger.getGlobal().info(savePath.getAbsolutePath());
+                jfield.setText(savePath.getAbsolutePath());
+            } else {
+                Logger.getGlobal().info("Cancel Path Choosing");
+            }
+        }
+
     }
 
     private static void createAndShowGUI(){
@@ -33,6 +63,7 @@ public class UIDownloader extends JPanel
     }
 
     public static void main(String[] args){
+//        Logger.getGlobal().setLevel(Level.OFF);
 
         SwingUtilities.invokeLater(new Runnable() {
             @Override
